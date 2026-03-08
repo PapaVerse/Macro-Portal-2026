564912a82c818762117bc0c9fef67029317f9f0d
John Paul Valdez <valconfi1212@gmail.com>
2026-03-08 08:18:16 +0800

    added favicon in admin

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,3 +1,4 @@
+<link rel="icon" type="image/png" href="{{ asset('images/M.png') }}">
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
